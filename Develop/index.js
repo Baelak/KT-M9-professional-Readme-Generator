f722cc2f20366de08451349d6dd90fca196ea8ec
Baelak <teklemichaelkaleab@gmail.com>
2024-08-03 10:24:59 -0400

    created readme questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,86 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter your Project title",
+    
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of your Project",
+    
+    },
+    {
+        type: "input",
+        name: "link",
+        message: "Please enter a URL for your Project",
+    
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Please enter licenses used for your Project",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+    
+    },
+    {
+        type: "input",
+        name: "require",
+        message: "Please enter dependencies for your Project",
+    
+    },
+    {
+        type: "input",
+        name: "features",
+        message: "Please enter features of your Project",
+    
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter languages and technologies used for your Project",
+    
+    },
+    {
+        type: "input",
+        name: "creator",
+        message: "Please enter your GitHub username",
+    
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your Email address",
+    
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Please enter any contributor's GitHub usernames",
+        default: "",
+    
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please enter walkthrough of required tests if applicable",
+    
+    },
+    {
+        type: "prompt",
+        name: "questions",
+        message: "Please feel free to email me at teklemichaelkaleab@gmail.com if you have any questions",
+    
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
